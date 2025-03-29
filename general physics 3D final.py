fb070bf55062fb500eb8b69e663d7e5db116660e
AnastasiosKousis <anastasios.kousis@student.manchester.ac.uk>
2025-03-29 13:15:40 +0000

    Added heat coefficient

--- general physics 3D final.py
+++ general physics 3D final.py
@@ -22,6 +22,7 @@ alpha = k / (rho * c_p)  # Thermal diffusivity (m²/s)
 Initial_temp = 283.0  # Temperature (K)
 R_steak = 8.314
 M_steak = 0.02897
+kappa = 1000
 
 # Radiation Cooling Parameters
 epsilon = 0.95    # Emissivity of surface
@@ -74,7 +75,7 @@ def air_density(h):
         P = 0.374 * np.exp(-G * (h - 86000) / (R_air * Temp))
 
     elif h < 110000:
-        Temp = 263.1905 - 76.32321 * (1 - np.sqrt(1 - ((h - 91000) / 19942.9)**2))
+        Temp = 263.1905 - 76.32321 * (np.sqrt(1 - ((h - 91000) / 19942.9)**2))
         P = 0.374 * np.exp(-G * (h - 86000) / (R_air * Temp))
 
     elif h < 120000:
@@ -116,7 +117,7 @@ def ambient_temp(h):
         Temp = 186.8673
 
     elif h < 110000:  # Thermosphere (91 - 110 km)
-        Temp = 263.1905 - 76.32321 * (1 - np.sqrt(1 - (h - 91000) / 19942.9))
+        Temp = 263.1905 - 76.32321 * (np.sqrt(1 - ((h - 91000) / 19942.9)**2))
 
     elif h < 120000:  # Thermosphere (110 - 120 km)
         Temp = 240 + 0.012 * (h - 110000)
@@ -127,8 +128,13 @@ def ambient_temp(h):
 
     return Temp
 
+def heat_coefficient(v, h):
+    rho_air = air_density(h)
+    return 6.7*rho_air**(4/5)*abs(v)**(4/5)
+
 def heat(T, v, h):
-    q = k*(ambient_temp(h)*(1+(v/(np.sqrt(gamma*R_steak*ambient_temp(h)/M_steak)))**2*(gamma-1)/2)-T)
+    kappa = heat_coefficient(v, h)
+    q = kappa*(ambient_temp(h)*(1+(v/(np.sqrt(gamma*R_steak*ambient_temp(h)/M_steak)))**2*(gamma-1)/2)-T)
     return q
 
 def rad_cooling(T, h):
@@ -149,7 +155,6 @@ T[1:-1, 1:-1, 0] += (rad_cooling(T[1:-1,1:-1,0], heights[0]) * dt) / (dz*rho * c
 T[1:-1, 1:-1, -1] += (rad_cooling(T[1:-1,1:-1,-1], heights[0]) * dt) / (dz*rho * c_p) # Back
 
 T_max_arr = T
-T_central_max = 0
 counter = 0
 
 # Time-stepping loop
@@ -165,6 +170,11 @@ for t in range(time_steps):
                     (T[i, j+1, k] - 2*T[i, j, k] + T[i, j-1, k]) / dy**2 +
                     (T[i, j, k+1] - 2*T[i, j, k] + T[i, j, k-1]) / dz**2
                 )
+    
+    # Find at what step max internal temp is reached
+    if T_new[nx//2, ny//2, nz//2] > T_max_arr[nx//2, ny//2, nz//2]:
+        step = t
+    
     # Update max temperature array
     np.maximum(T, T_max_arr, out=T_max_arr)
     
@@ -173,21 +183,21 @@ for t in range(time_steps):
         T_quarter = T_new.copy()
         print("25%")
         plt.figure(figsize=(6, 5))
-        plt.imshow(T_quarter[:,:,nz//2], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T_quarter[:,:,nz//2], cmap="hot", origin="lower", extent=[0,ny*dy,0,nx*dx])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (25%)")
         plt.xlabel("x (m)")
         plt.ylabel("y (m)")
         plt.show()
         plt.figure(figsize=(6, 5))
-        plt.imshow(T_quarter[nx//2,:,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T_quarter[nx//2,:,:], cmap="hot", origin="lower", extent=[0,ny*dy,0,nz*dz])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (25%)")
         plt.xlabel("x (m)")
         plt.ylabel("y (m)")
         plt.show()
         plt.figure(figsize=(6, 5))
-        plt.imshow(T_quarter[:,ny//2,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T_quarter[:,ny//2,:], cmap="hot", origin="lower", extent=[0,nz*dz,0,nx*dx])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (25%)")
         plt.xlabel("x (m)")
@@ -199,21 +209,21 @@ for t in range(time_steps):
         T_half = T_new.copy()
         print("50%")
         plt.figure(figsize=(6, 5))
-        plt.imshow(T_half[:,:,nz//2], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T_half[:,:,nz//2], cmap="hot", origin="lower", extent=[0,ny*dy,0,nx*dx])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (50%)")
         plt.xlabel("x (m)")
         plt.ylabel("y (m)")
         plt.show()
         plt.figure(figsize=(6, 5))
-        plt.imshow(T_half[nx//2,:,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T_half[nx//2,:,:], cmap="hot", origin="lower", extent=[0,ny*dy,0,nz*dz])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (50%)")
         plt.xlabel("x (m)")
         plt.ylabel("y (m)")
         plt.show()
         plt.figure(figsize=(6, 5))
-        plt.imshow(T_half[:,ny//2,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T_half[:,ny//2,:], cmap="hot", origin="lower", extent=[0,nz*dz,0,nx*dx])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (50%)")
         plt.xlabel("x (m)")
@@ -225,31 +235,27 @@ for t in range(time_steps):
         T_3_quarter = T_new.copy()
         print("75%")
         plt.figure(figsize=(6, 5))
-        plt.imshow(T_3_quarter[:,:,nz//2], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T_3_quarter[:,:,nz//2], cmap="hot", origin="lower", extent=[0,ny*dy,0,nx*dx])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (75%)")
         plt.xlabel("x (m)")
         plt.ylabel("y (m)")
         plt.show()
         plt.figure(figsize=(6, 5))
-        plt.imshow(T_3_quarter[nx//2,:,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T_3_quarter[nx//2,:,:], cmap="hot", origin="lower", extent=[0,ny*dy,0,nz*dz])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (75%)")
         plt.xlabel("x (m)")
         plt.ylabel("y (m)")
         plt.show()
         plt.figure(figsize=(6, 5))
-        plt.imshow(T_3_quarter[:,ny//2,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T_3_quarter[:,ny//2,:], cmap="hot", origin="lower", extent=[0,nz*dz,0,nx*dx])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (75%)")
         plt.xlabel("x (m)")
         plt.ylabel("y (m)")
         plt.show()
     
-     
-    # Find at what step max internal temp is reached
-    if T[nx//2, ny//2, nz//2] > T_central_max:
-        step = t
     
     # Rare
     if counter==0 and T[nx//2, ny//2, nz//2]>323:
@@ -257,7 +263,7 @@ for t in range(time_steps):
         print(f"Step: {t}, Height: {heights[t]}")
         counter+=1
         plt.figure(figsize=(6, 5))
-        plt.imshow(T[nx//2,:,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T[nx//2,:,:], cmap="hot", origin="lower", extent=[0,ny*dy,0,nz*dz])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (Rare)")
         plt.xlabel("x (m)")
@@ -270,7 +276,7 @@ for t in range(time_steps):
         print(f"Step: {t}, Height: {heights[t]}")
         counter+=1
         plt.figure(figsize=(6, 5))
-        plt.imshow(T[nx//2,:,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T[nx//2,:,:], cmap="hot", origin="lower", extent=[0,ny*dy,0,nz*dz])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (Medium Rare)")
         plt.xlabel("x (m)")
@@ -283,7 +289,7 @@ for t in range(time_steps):
         print(f"Step: {t}, Height: {heights[t]}")
         counter+=1
         plt.figure(figsize=(6, 5))
-        plt.imshow(T[nx//2,:,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T[nx//2,:,:], cmap="hot", origin="lower", extent=[0,ny*dy,0,nz*dz])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (Medium)")
         plt.xlabel("x (m)")
@@ -296,7 +302,7 @@ for t in range(time_steps):
         print(f"Step: {t}, Height: {heights[t]}")
         counter+=1
         plt.figure(figsize=(6, 5))
-        plt.imshow(T[nx//2,:,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T[nx//2,:,:], cmap="hot", origin="lower", extent=[0,ny*dy,0,nz*dz])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (Medium Well)")
         plt.xlabel("x (m)")
@@ -309,7 +315,7 @@ for t in range(time_steps):
         print(f"Step: {t}, Height: {heights[t]}")
         counter+=1
         plt.figure(figsize=(6, 5))
-        plt.imshow(T[nx//2,:,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+        plt.imshow(T[nx//2,:,:], cmap="hot", origin="lower", extent=[0,ny*dy,0,nz*dz])
         plt.colorbar(label="Temperature (K)")
         plt.title("2D Steak cross-section (Well done)")
         plt.xlabel("x (m)")
@@ -317,7 +323,7 @@ for t in range(time_steps):
         plt.show()
         
     # Heat transfers
-    q_dot_t = heat(T[0,:,:],velocities[t], heights[t]) * dt/ (dz*rho * c_p)
+    q_dot_t = heat(T[0,:,:],velocities[t], heights[t]) * dt/ (dz * rho * c_p)
     T_new[0, :, :] += q_dot_t +(rad_cooling(T[0,:,:], heights[t]) * dt) / (dz*rho * c_p)  # Left
     T_new[-1, :, :] += (rad_cooling(T[-1,:,:], heights[t]) * dt) / (dz*rho * c_p)  # Right
     T_new[1:-1, 0, :] += (rad_cooling(T[1:-1,0,:], heights[t]) * dt) / (dz*rho * c_p)  # Bottom
@@ -329,24 +335,27 @@ for t in range(time_steps):
     # Update temperature field
     T = T_new.copy()
 
-# Plot Final Results
-print(f"Max central temperature: {T_central_max}")
+
+print(f"Total steps: {time_steps}")
+print(f"Max central temperature: {T_max_arr[nx//2, ny//2, nz//2]}")
 print(f"Step: {step}")
+
+# Plot Final Results
 plt.figure(figsize=(6, 5))
-plt.imshow(T[nx//2,:,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+plt.imshow(T[nx//2,:,:], cmap="hot", origin="lower", extent=[0,ny*dy,0,nz*dz])
 plt.colorbar(label="Temperature (K)")
 plt.title("2D Steak cross-section")
 plt.xlabel("x (m)")
 plt.ylabel("y (m)")
 plt.show()
 plt.figure(figsize=(6, 5))
-plt.imshow(T[:,:,nz//2], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+plt.imshow(T[:,:,nz//2], cmap="hot", origin="lower", extent=[0,ny*dy,0,nx*dx])
 plt.colorbar(label="Temperature (K)")
 plt.title("2D Steak cross-section")
 plt.xlabel("x (m)")
 plt.ylabel("y (m)")
 plt.show()
-plt.imshow(T[:,ny//2,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+plt.imshow(T[:,ny//2,:], cmap="hot", origin="lower", extent=[0,nz*dz,0,nx*dx])
 plt.colorbar(label="Temperature (K)")
 plt.title("2D Steak cross-section")
 plt.xlabel("x (m)")
@@ -354,21 +363,21 @@ plt.ylabel("y (m)")
 plt.show()
 
 # Plot max temperature array
-plt.imshow(T_max_arr[nx//2,:,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+plt.imshow(T_max_arr[nx//2,:,:], cmap="hot", origin="lower", extent=[0,ny*dy,0,nz*dz])
 plt.colorbar(label="Temperature (K)")
-plt.title("2D Steak cross-section (Max reached temperature)")
+plt.title("2D Steak cross-section (Max temperature)")
 plt.xlabel("x (m)")
 plt.ylabel("y (m)")
 plt.show()
-plt.imshow(T_max_arr[:,ny//2,:], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+plt.imshow(T_max_arr[:,ny//2,:], cmap="hot", origin="lower", extent=[0,nz*dz,0,nx*dx])
 plt.colorbar(label="Temperature (K)")
-plt.title("2D Steak cross-section (Max reached temperature)")
+plt.title("2D Steak cross-section (Max temperature)")
 plt.xlabel("x (m)")
 plt.ylabel("y (m)")
 plt.show()
-plt.imshow(T_max_arr[:,:,nz//2], cmap="hot", origin="lower", extent=[0, nx*dx, 0, ny*dy])
+plt.imshow(T_max_arr[:,:,nz//2], cmap="hot", origin="lower", extent=[0,ny*dy,0,nx*dx])
 plt.colorbar(label="Temperature (K)")
-plt.title("2D Steak cross-section (Max reached temperature)")
+plt.title("2D Steak cross-section (Max temperature)")
 plt.xlabel("x (m)")
 plt.ylabel("y (m)")
 plt.show()
